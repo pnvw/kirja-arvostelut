2127d46dc685a7e834b0a03edf7119e81bf1d68d
Priscilla <priscilla.viitanen@gmail.com>
2026-03-26 22:18:23 +0200

    Add new reviw form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    book_type TEXT,
+    book_name TEXT,
+    writer TEXT,
+    review TEXT,
+    grade INTEGER,
+    user_id INTEGER REFERENCES users
+);
